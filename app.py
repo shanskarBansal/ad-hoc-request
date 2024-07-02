69dc0e247a327e99f55a77dc445fafa597e739b5
shanskarBansal <115683341+shanskarBansal@users.noreply.github.com>
2024-07-02 17:18:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -473,20 +473,8 @@ def app_second_block():
             wks_write.frozen_rows = 1
 
 
-    Google_api_credential_dict = {
-        "type": st.secrets["gcp_service_account"]["type"],
-        "project_id": st.secrets["gcp_service_account"]["project_id"],
-        "private_key_id": st.secrets["gcp_service_account"]["private_key_id"],
-        "private_key": st.secrets["gcp_service_account"]["private_key"],
-        "client_email": st.secrets["gcp_service_account"]["client_email"],
-        "client_id": st.secrets["gcp_service_account"]["client_id"],
-        "auth_uri": st.secrets["gcp_service_account"]["auth_uri"],
-        "token_uri": st.secrets["gcp_service_account"]["token_uri"],
-        "auth_provider_x509_cert_url": st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
-        "client_x509_cert_url": st.secrets["gcp_service_account"]["client_x509_cert_url"]
-    }
+    Google_api_credential_dict = = dict(st.secrets["gcp_service_account"])
 
-    
     credentials = service_account.Credentials.from_service_account_info(Google_api_credential_dict)
     google_api_object = google_api_class(
         credential_file=credentials)
