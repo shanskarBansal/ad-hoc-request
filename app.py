991fdbaf089d99c6dcf3858be1d69211560f923e
shanskarBansal <115683341+shanskarBansal@users.noreply.github.com>
2024-08-01 15:40:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -592,8 +592,7 @@ def app_second_block():
                                                                 api_key=api_key, start_date=start_date,
                                                                 end_date=end_date, profile_type_df=profile_type_df,
                                                                 profile_name=profile_name)
-        print(final_df.columns)
-
+        print(final_df)
         final_df['Created time'] = pd.to_datetime(final_df['Created time'])
         add_time = timedelta(hours=3, minutes=30)
         final_df['Created time'] = final_df['Created time'] + add_time
